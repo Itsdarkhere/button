a924dde0ed94c64fa6d361e3b64c5b7852c947e1
Valtteri Juvonen <57869878+Itsdarkhere@users.noreply.github.com>
2024-09-06 14:49:57 +0300

    n

--- components/Joystick.tsx
+++ components/Joystick.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useCallback, useRef, useEffect } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { motion } from 'framer-motion';
 
 export default function Joystick() {
